b4113a162182443ef4e8a50546c54d375d2aae11
Golfring <golfring.dove@hotmail.com>
2025-10-04 16:01:27 +0700

    create ver. java.util.queue

--- src/javautilQueue/Face2.java
+++ src/javautilQueue/Face2.java
@@ -2,21 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package travelbagsqueue;
-
+package javautilQueue;
+import travelbagsqueue.*;
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author Windows10
  */
-public class Face extends javax.swing.JFrame {
+public class Face2 extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Face.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Face2.class.getName());
 
     /**
      * Creates new form Face
      */
-    public Face() {
+    public Face2() {
         initComponents();
+        this.getContentPane().setBackground(new Color(98, 149, 150));
+        UIManager.put("OptionPane.messageFont", new Font("Tahoma", Font.PLAIN, 16));
+        UIManager.put("OptionPane.buttonFont", new Font("Tahoma", Font.PLAIN, 14));
+        ImageIcon icon = new ImageIcon("intro.gif");  // gif ที่ตั้ง loop=1
+    JLabel label = new JLabel(icon);
+    
+    Timer timer = new Timer(12000, e -> {
+            Body move = new Body();  // สร้างหน้าฟอร์มใหม่
+            move.setVisible(true);     // แสดง Form2
+            this.dispose();     // เปิดหน้าหลัก
+        });
+            timer.setRepeats(false);  // ทำงานครั้งเดียว
+            timer.start();
     }
 
     /**
@@ -28,39 +45,27 @@ public class Face extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/planepic.png"))); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/ezgif.com-animated-gif-maker.gif"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(11, Short.MAX_VALUE))
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jButton1)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Face2 move = new Face2();  // สร้างหน้าฟอร์มใหม่
-            move.setVisible(true);     // แสดง Form2
-            this.dispose(); 
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -84,10 +89,10 @@ public class Face extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new Face().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new Face2().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
